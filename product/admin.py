0bb1e2cadbdbe7116103df03c5ed4d96d8055b23
Ataiskih <aziankg@gmail.com>
2020-10-14 20:22:43 +0600

    added in product page: additional images, product charecteristics; in admin: to able adding additional images and charesteristic

--- product/admin.py
+++ product/admin.py
@@ -2,14 +2,30 @@ from django.contrib import admin
 from product.models import (
     Product,
     ProductCharacteristic,
+    ProductAdditionalImages,
 )
 
+
+class ProductAdditionalImagesAdminInline(admin.TabularInline):
+    model = ProductAdditionalImages
+    fields = ['addentional_images',]
+    extra = 1
+
+
+class ProductCharacteristicAdminInline(admin.TabularInline):
+    model = ProductCharacteristic
+    fields = ['material', 'size', 'color']
+    extra = 0
 
 @admin.register(Product)
 class ProductAdminForm(admin.ModelAdmin):
     exclude = ('updated',)
+    inlines = [
+        ProductAdditionalImagesAdminInline,
+        ProductCharacteristicAdminInline,
+    ]
+    
 
-
-@admin.register(ProductCharacteristic)
-class ProductAdminForm(admin.ModelAdmin):
-    exclude = ('product',)
+# @admin.register(ProductCharacteristic)
+# class ProductAdminForm(admin.ModelAdmin):
+#     list_display = ['material', 'size', 'color']
