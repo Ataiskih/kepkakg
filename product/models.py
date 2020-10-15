0c5bfa755130c9bb7f642d8960a9f9c66331042f
Ataiskih <aziankg@gmail.com>
2020-10-15 23:21:07 +0600

    added in admin: user profiles join with user in admin

--- product/models.py
+++ product/models.py
@@ -1,32 +1,5 @@
 from django.db import models
-
-
-class BaseAbstractModel(models.Model):
-    name = models.CharField(
-        max_length=255,
-        blank=True, null=True,
-        verbose_name='Название'
-    )
-    created = models.DateField(
-        auto_now_add=True,
-        verbose_name='Дата создания'
-    )
-    updated = models.DateField(
-        auto_now=True,
-        verbose_name='Дата изменения'
-    )
-    deleted = models.BooleanField(
-        default=False,
-        verbose_name='Удален'
-    )
-
-    def __str__(self):
-        if self.name:
-            return self.name
-        return f"{self.pk}"
-
-    class Meta:
-        abstract = True
+from base.models import BaseAbstractModel
 
 
 class Product(BaseAbstractModel):
--- users_profiles/admin.py
+++ users_profiles/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from users_profiles.models import UserProfile
 
-# Register your models here.
+
+class UserProfileAdmin(admin.StackedInline):
+    model = UserProfile
+    exclude = ['updated',]
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (UserProfileAdmin,)
+    exclude = ['updated',]
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
