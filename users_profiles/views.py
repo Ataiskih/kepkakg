63da02d647812c3d35cfd54cb86f40e9ec41a2bb
Ataiskih <aziankg@gmail.com>
2020-10-23 03:08:48 +0600

    POST to edit profile with bags 403

--- users_profiles/views.py
+++ users_profiles/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.views.generic import (
     ListView,
+    UpdateView,
 )
 from users_profiles.models import UserProfile
 
@@ -31,3 +32,22 @@ class UserProfileView(ListView):
     #         username=username
     #     )
     #     return context
+
+class UserProfileUpdate(UpdateView):
+    model = UserProfile
+    fields = [
+        'user',
+        'first_name',
+        'last_name',
+        'avatar',
+        'gender',
+        'birthday',
+        'email',
+        'phone',
+    ]
+    
+    template_name = 'user_profiles/profiles.html'
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        return super().post(request, *args, **kwargs)
